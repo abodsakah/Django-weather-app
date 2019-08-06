6436df4ac492d432f095cd7315cefe054192d676
abodsakka <hloarab@gmail.com>
2019-08-06 19:24:44 +0200

    ver 1.0

--- weather_app/weather/urls.py
+++ weather_app/weather/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='Home'),
+    path('delete/<city_name>', views.delete_city, name='delete_city')
 ]
